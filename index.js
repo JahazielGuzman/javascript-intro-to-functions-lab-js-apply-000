d67da1a9f05bc2c83a62e043eb1fd8a71d042fb2
Jahaziel Guzman <jahaziel.guzman93@gmail.com>
2019-05-21 02:39:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function whisper(string) {
 
 function logShout(string) {
   
-  return console.log(string.toUpperCase);
+  return console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
